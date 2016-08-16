60c48a5a50761be5d71b24697705237279a8f765
Weston Ruter <weston@xwp.co>
2016-08-15 21:45:32 -0700

    Add phpunit test stub

---
+++ tests/php/test-class-customize-concurrency.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Test Plugin.
+ *
+ * @package CustomizeConcurrency
+ */
+
+namespace CustomizeConcurrency;
+
+/**
+ * Class Test_Customize_Concurrency
+ */
+class Test_Customize_Concurrency extends \WP_UnitTestCase {
+
+	/**
+	 * Test constructor.
+	 *
+	 * @see Customize_Concurrency::__construct()
+	 */
+	function test_construct() {
+		$this->markTestIncomplete();
+	}
+}
